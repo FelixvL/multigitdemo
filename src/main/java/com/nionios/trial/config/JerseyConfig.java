6c3a58db64e12a0ee0a14a7aac063967932a25de
Nikolopoulos <JM86DV@ad.ing.net>
2018-09-06 13:36:51 +0200

    first commit Springbot

---
+++ src/main/java/com/nionios/trial/config/JerseyConfig.java
@@ -0,0 +1,17 @@
+package com.nionios.trial.config;
+
+import javax.ws.rs.ApplicationPath;
+
+import org.glassfish.jersey.server.ResourceConfig;
+import org.springframework.stereotype.Component;
+
+import com.nionios.trial.api.PersonEndpoint;
+
+
+@Component
+@ApplicationPath("/api")
+public class JerseyConfig extends ResourceConfig {
+    public JerseyConfig(){
+        register(PersonEndpoint.class);
+    }
+}
---
+++ src/main/java/com/nionios/trial/controller/PersonService.java
@@ -0,0 +1,36 @@
+package com.nionios.trial.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.nionios.trial.domain.Person;
+
+@Service
+@Transactional
+public class PersonService {
+
+    @Autowired
+    private PersonRepository personRepository;
+
+    public Person save(Person person) {
+        return personRepository.save(person);
+    }
+
+    public void deletePerson(Long id) {
+        personRepository.deleteById(id);
+    }
+/*
+    public Person updatePerson(Person person, Long id, String field) {
+       return personRepository.findById(id).get().
+    }
+*/
+    public Person findById(Long id) {
+        return personRepository.findById(id).get();
+    }
+
+    public Iterable<Person> findAll() {
+        Iterable<Person> result = personRepository.findAll();
+        return result;
+    }
+}
